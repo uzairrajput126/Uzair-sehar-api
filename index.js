e5483722d02571b6b70a108794e84a4b8679f195
uzairrajput126 <uzairrajput126@gmail.com>
2025-06-07 22:26:16 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = 10000;
 
 app.get("/ytdip", async (req, res) => {
     const { link, format } = req.query;
